3a3bd3d32b61454db4e38fb4abc94aeecc20b465
root <root@host.chmviola.com.br>
2025-12-28 19:25:28 -0300

    Travel manager v_0.022 - Alteração de tempo sleep no arquivo Jenkinsfile-dev

--- app/config/settings.py
+++ app/config/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from decouple import config
 
 # Versão do Aplicativo (Edite apenas aqui!)
-APP_VERSION = '0.0.21'
+APP_VERSION = '0.0.22'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
