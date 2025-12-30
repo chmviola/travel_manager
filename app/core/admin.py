af3cd2639c5f7e12ac4132ab23a55c2cd153313e
Carlos Henrique <chmviola@Carloss-MacBook-Air.local>
2025-12-30 10:39:13 -0300

    Tavel Manager v_0042 - Disponibilização das tabelas no módulo admin do Django

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import Trip, TripItem, Expense
+from .models import Trip, TripItem, TripAttachment, Expense, APIConfiguration
 
 admin.site.register(Trip)
+
 admin.site.register(TripItem)
+
+admin.site.register(TripAttachment)
+
 admin.site.register(Expense)
+
+admin.site.register(APIConfiguration)
+class APIConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('get_key_display', 'is_active', 'updated_at')
+    list_filter = ('is_active', 'key')
+    search_fields = ('key', 'description')
+    
+    # Isso melhora a visualização do nome do serviço na lista
+    def get_key_display(self, obj):
+        return obj.get_key_display()
+    get_key_display.short_description = 'Serviço / API'
