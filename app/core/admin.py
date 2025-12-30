dd7aa186665b0be5677d9bfb764f46d3c4585859
root <root@host.chmviola.com.br>
2025-12-30 13:47:09 -0300

    Tavel Manager v_0044 - Integração com a OpenAI (Checklist de Bagagem)

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Trip, TripItem, TripAttachment, Expense, APIConfiguration
+from .models import Trip, TripItem, TripAttachment, Expense, APIConfiguration, Checklist, ChecklistItem
 
 admin.site.register(Trip)
 
@@ -8,6 +8,10 @@ admin.site.register(TripItem)
 admin.site.register(TripAttachment)
 
 admin.site.register(Expense)
+
+admin.site.register(Checklist)
+
+admin.site.register(ChecklistItem)
 
 admin.site.register(APIConfiguration)
 class APIConfigurationAdmin(admin.ModelAdmin):
