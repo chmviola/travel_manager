0e8f874f46433aba40022981277660e9222fc9cc
Carlos Henrique <chmviola@Carloss-MacBook-Air.local>
2025-12-31 00:57:37 -0300

    Tavel Manager v_0056 - Reparo na função de Geração de timeline por IA

--- app/core/admin.py
+++ app/core/admin.py
@@ -13,13 +13,7 @@ admin.site.register(Checklist)
 
 admin.site.register(ChecklistItem)
 
-admin.site.register(APIConfiguration)
+@admin.register(APIConfiguration)
 class APIConfigurationAdmin(admin.ModelAdmin):
-    list_display = ('get_key_display', 'is_active', 'updated_at')
-    list_filter = ('is_active', 'key')
-    search_fields = ('key', 'description')
-    
-    # Isso melhora a visualização do nome do serviço na lista
-    def get_key_display(self, obj):
-        return obj.get_key_display()
-    get_key_display.short_description = 'Serviço / API'
+    list_display = ('key', 'value', 'is_active')
+    search_fields = ('key',)
